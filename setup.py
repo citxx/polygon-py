411f454d5d894319712e9b4bf4d3a15861e2f08e
Artem Tabolin <artemtab@gmail.com>
2019-09-14 19:30:06 +0300

    Rename package to polygon_client and add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polygon_client",
+    version="1.0a1",
+    author="Artem Tabolin",
+    author_email="artemtab@gmail.com",
+    description="Python wrapper for Polygon API (polygon.codeforces.com)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/citxx/polygon-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
