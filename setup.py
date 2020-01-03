5b71d0d56005f94203382d465b042e657c8b151f
Artem Tabolin <artemtab@gmail.com>
2020-01-03 16:31:21 +0300

    Lower the requirement to Python version to 3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="polygon_client",
-    version="1.0a4",
+    version="1.0a5",
     author="Artem Tabolin",
     author_email="artemtab@gmail.com",
     description="Python wrapper for Polygon API (polygon.codeforces.com)",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
