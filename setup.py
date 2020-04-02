449d3871aff432aa5eb501a215546e8de0f50e6e
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2020-04-02 19:05:48 +0300

    Add requests dependency

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/citxx/polygon-py",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
