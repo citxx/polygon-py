8ba3c46c1bf56e26532d6fa8f534201908aefcdc
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2023-12-06 16:50:12 +0200

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="polygon_api",
-    version="1.0.0",
+    version="1.0.1",
     author="Artem Tabolin",
     author_email="artemtab@gmail.com",
     description="Python wrapper for Polygon API (polygon.codeforces.com)",
